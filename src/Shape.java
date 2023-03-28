2596265f7c052f546d21aaa90a2aaf309f2c4bca
BT_3W2_21 <alps2789@nycstudents.net>
2023-03-28 11:50:47 -0400

    done!

--- src/Shape.java
+++ src/Shape.java
@@ -10,6 +10,8 @@ public class Shape {
   // public constants
   public static final String RECTANGLE = "Rectangle"; // constant for rectangle
   public static final String OVAL = "Oval";           // constant for oval
+  public static final String ARC = "Arc";
+  public static final String LINE = "Line";
 
   // private instance variables
   private Color color; // color to draw shape in
@@ -18,7 +20,7 @@ public class Shape {
 
   /** Constructor */
   public Shape() {
-    color = Color.black;
+    color = Color.green;
     p1 = new Point();
     p2 = new Point();
   }
@@ -26,6 +28,10 @@ public class Shape {
   public Color getColor() {
     return color;
   }
+
+  public Point getP1() { return p1; }
+  public Point getP2() { return p2; }
+
 
   /**
    * Method to get minimum x value of the bounding rectangle
